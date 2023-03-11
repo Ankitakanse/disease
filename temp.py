89576334c1215f5b6f368fc36456d0613370f376
Ankitakanse <114207806+Ankitakanse@users.noreply.github.com>
2023-03-11 21:42:24 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -12,11 +12,11 @@ from streamlit_option_menu import option_menu
 
 # loading the saved models
 
-diabetes_model = pickle.load(open('C:/Users/ASK Kanse/Downloads/diabetes_model.sav', 'rb'))
+diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
 
-heart_disease_model = pickle.load(open('C:/Users/ASK Kanse/Downloads/heart_disease_model.sav','rb'))
+heart_disease_model = pickle.load(open('heart_disease_model.sav','rb'))
 
-parkinsons_model = pickle.load(open('C:/Users/ASK Kanse/Downloads/parkinsons_model.sav', 'rb'))
+parkinsons_model = pickle.load(open('parkinsons_model.sav', 'rb'))
 
 
 
